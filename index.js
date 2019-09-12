a8cf58b82fcad417a529318ba91745193a882764
Levi Miller <paullevimiller@gmail.com>
2019-09-12 10:29:03 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objArr) {
+  const winData =  objArr.find(obj => obj["result"] === "W" );
+  return winData === undefined ? (winData) : (winData["year"]);
+};
